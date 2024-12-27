fe5d2221f7ab537006e280c582293f54a50b6fc5
Daniela Bodner <daniela.bodner@iu-study.org>
2024-12-27 12:07:47 +0100

    Implementierung App-Header und HomeView

--- quizius-maximus/src/App.vue
+++ quizius-maximus/src/App.vue
@@ -1,11 +1,11 @@
 <script setup>
 import { RouterView } from "vue-router";
+import AppHeader from "./components/AppHeader.vue";
 </script>
 
 <template>
-  <main>
-    <RouterView />
-  </main>
+  <AppHeader />
+  <RouterView />
 </template>
 
 <style scoped></style>
--- quizius-maximus/src/views/Home.vue
+++ quizius-maximus/src/views/Home.vue
@@ -1,7 +1,35 @@
-<script setup></script>
+<script setup>
+import { ref, onMounted } from 'vue';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import router from '@/router';
+import LogoutButton from '@/components/LogoutButton.vue';
+
+const userLoggedIn = ref(false);
+const userName = ref("");
+
+/** check whether user is logged in when loading this view*/
+onMounted(() => {
+    onAuthStateChanged(getAuth(), function (user) {
+        if (!user) {
+            userLoggedIn.value = false;
+            router.push("/login");
+        } else {
+            userLoggedIn.value = true;
+            // TODO: Alternative finden, hier ist der Name leer, weil Profil-Update erst nach Auth-Creation ... onAuthStateChanged triggert schneller
+            // => nach initialer Username-Vergabe leer, ab dann immer gefüllt
+            userName.value = user.displayName;
+        }
+    });
+});
+</script>
 
 <template>
-    <h1 class="text-center">Startseite</h1>
+    <main class="d-flex justify-content-center">
+        <fieldset v-if="userLoggedIn">
+            <legend>Willkommen zurück {{ userName }}!</legend>
+            <div class="d-flex justify-content-center">
+                <LogoutButton />
+            </div>
+        </fieldset>
+    </main>
 </template>
-
-<style scoped></style>
