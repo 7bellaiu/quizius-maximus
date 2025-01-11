c3db237f97f395a36e9ba47f4411361e5a9a06e1
Daniela Bodner <daniela.bodner@iu-study.org>
2025-01-11 12:15:53 +0100

    Button zu Router-Link umfunktioniert

--- quizius-maximus/src/components/ModuleCard.vue
+++ quizius-maximus/src/components/ModuleCard.vue
@@ -25,12 +25,13 @@ const props = defineProps({
                 <h6 class="card-subtitle">{{ props.longname }}</h6>
                 <p class="card-text">Hier könnte zusätzlich noch ein Beschreibungstext rein.</p>
                 <div class="d-flex justify-content-center align-items-center">
-                    <button type="button" class="btn btn-sm btn-outline-danger w-25 mx-2">
+                    <router-link class="btn btn-sm btn-outline-danger w-25 mx-2" to="/compgamemodes">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                             class="bi bi-person-fill" viewBox="0 0 16 16">
                             <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6"></path>
                         </svg>
-                    </button>
+                    </router-link>
+                    <!-- TODO: router link für coop gamemodes -->
                     <button type="button" class="btn btn-sm btn-outline-success w-25 mx-2">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                             class="bi bi-people-fill" viewBox="0 0 16 16">
