019f43ee976df6ab6e2e50bd19703b64638abfc8
Daniela Bodner <Daniela.Bodner@iu-study.org>
2025-01-16 15:29:36 +0100

    CI-Anpassung Questionnaires.vue: Erklärung für (+)-Button rausgenommen und Färbung der Wörter angepasst

--- quizius-maximus/src/views/Questionnaires.vue
+++ quizius-maximus/src/views/Questionnaires.vue
@@ -102,23 +102,8 @@ const deleteQuestionnaire = async (moduleId) => {
                                 <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M4.5 7.5a.5.5 0 0 0 0 1h7a.5.5 0 0 0 0-1z" />
                             </svg>
                         </span>
-                        löscht den
-                        <span class="text-danger"> gesamten </span>
-                        Fragenkatalog eines Moduls
-                    </p>
-                    <p class="lead text-body-secondary">
-                        <span class="btn btn-outline-success">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                                class="bi bi-plus-circle-fill" viewBox="0 0 16 16">
-                                <path
-                                    d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3z" />
-                            </svg>
-                        </span>
-                        erstellt einen
-                        <span class="text-success">
-                            neuen
-                        </span>
-                        Fragenkatalog (neues Modul)
+                        <span class="text-danger"> löscht </span>
+                        den gesamten Fragenkatalog eines Moduls
                     </p>
                     <p class="lead text-body-secondary">
                         <span class="btn btn-outline-warning">
