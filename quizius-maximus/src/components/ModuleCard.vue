c28a2fecae7c1565cf84b9ed1e18820ab90f6ea9
Daniela Bodner <Daniela.Bodner@iu-study.org>
2025-01-17 11:54:12 +0100

    CI: Endlich eine schöne Optik für die Module

--- quizius-maximus/src/components/ModuleCard.vue
+++ quizius-maximus/src/components/ModuleCard.vue
@@ -20,28 +20,107 @@ const props = defineProps({
     <div class="col">
         <div class="card shadow-sm border-primary">
             <div class="card-header text-bg-primary border-primary">
-                {{ props.shortname }}: {{ props.moduleid }}
+                {{ props.shortname }}: {{ props.longname }}
             </div>
-            <div class="card-body bg-primary bg-opacity-10">
-                <h6 class="card-subtitle">{{ props.longname }}</h6>
-                <p class="card-text">Hier könnte zusätzlich noch ein Beschreibungstext rein.</p>
-                <div class="d-flex justify-content-center align-items-center">
-                    <router-link class="btn btn-sm btn-outline-danger w-25 mx-2"
-                        :to="{ name: 'compgamemodes', params: { moduleId: props.moduleid, moduleShortname: props.shortname, moduleLongname: props.longname } }">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                            class="bi bi-person-fill" viewBox="0 0 16 16">
-                            <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6"></path>
-                        </svg>
-                    </router-link>
-                    <!-- TODO: router link für coop gamemodes -->
-                    <button type="button" class="btn btn-sm btn-outline-success w-25 mx-2">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                            class="bi bi-people-fill" viewBox="0 0 16 16">
-                            <path
-                                d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6m-5.784 6A2.24 2.24 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.3 6.3 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1zM4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5">
-                            </path>
-                        </svg>
-                    </button>
+            <div class="card-body">
+                <div class="row row-cols-2 mb-1">
+                    <div class="col-6">
+                        <small>Schnelles Quiz</small>
+                    </div>
+                    <div class="col">
+                        <div class="btn-toolbar justify-content-end">
+                            <router-link class="btn btn-sm btn-outline-danger mx-1"
+                                :to="{ name: 'game', params: { moduleId: props.moduleid, moduleShortname: props.shortname, moduleLongname: props.longname, gameMode: 'schnell' } }">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                    class="bi bi-person-fill" viewBox="0 0 16 16">
+                                    <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6">
+                                    </path>
+                                </svg>
+                            </router-link>
+                            <router-link type="button" class="btn btn-sm btn-outline-success mx-1" to="">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                    class="bi bi-people-fill" viewBox="0 0 16 16">
+                                    <path
+                                        d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6m-5.784 6A2.24 2.24 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.3 6.3 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1zM4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5">
+                                    </path>
+                                </svg>
+                            </router-link>
+                        </div>
+                    </div>
+                </div>
+                <div class="row row-cols-2 mb-1">
+                    <div class="col-6">
+                        <small>Themenbasiertes Quiz</small>
+                    </div>
+                    <div class="col">
+                        <div class="btn-toolbar justify-content-end">
+                            <router-link class="btn btn-sm btn-outline-danger mx-1"
+                                :to="{ name: 'game', params: { moduleId: props.moduleid, moduleShortname: props.shortname, moduleLongname: props.longname, gameMode: 'schnell' } }">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                    class="bi bi-person-fill" viewBox="0 0 16 16">
+                                    <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6">
+                                    </path>
+                                </svg>
+                            </router-link>
+                            <router-link type="button" class="btn btn-sm btn-outline-success mx-1" to="">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                    class="bi bi-people-fill" viewBox="0 0 16 16">
+                                    <path
+                                        d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6m-5.784 6A2.24 2.24 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.3 6.3 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1zM4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5">
+                                    </path>
+                                </svg>
+                            </router-link>
+                        </div>
+                    </div>
+                </div>
+                <div class="row row-cols-2 mb-1">
+                    <div class="col-6">
+                        <small>Prüfungssimulation</small>
+                    </div>
+                    <div class="col">
+                        <div class="btn-toolbar justify-content-end">
+                            <button class="btn btn-sm btn-outline-secondary mx-1" disabled>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                    class="bi bi-person-fill" viewBox="0 0 16 16">
+                                    <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6">
+                                    </path>
+                                </svg>
+                            </button>
+                            <router-link type="button" class="btn btn-sm btn-outline-success mx-1" to="">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                    class="bi bi-people-fill" viewBox="0 0 16 16">
+                                    <path
+                                        d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6m-5.784 6A2.24 2.24 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.3 6.3 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1zM4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5">
+                                    </path>
+                                </svg>
+                            </router-link>
+                        </div>
+                    </div>
+                </div>
+                <div class="row row-cols-2 mb-1">
+                    <div class="col-6">
+                        <small>Lernmodus</small>
+                    </div>
+                    <div class="col">
+                        <div class="btn-toolbar justify-content-end">
+                            <router-link class="btn btn-sm btn-outline-danger mx-1"
+                                :to="{ name: 'game', params: { moduleId: props.moduleid, moduleShortname: props.shortname, moduleLongname: props.longname, gameMode: 'schnell' } }">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                    class="bi bi-person-fill" viewBox="0 0 16 16">
+                                    <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6">
+                                    </path>
+                                </svg>
+                            </router-link>
+                            <button type="button" class="btn btn-sm btn-outline-secondary mx-1" disabled>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                    class="bi bi-people-fill" viewBox="0 0 16 16">
+                                    <path
+                                        d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6m-5.784 6A2.24 2.24 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.3 6.3 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1zM4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5">
+                                    </path>
+                                </svg>
+                            </button>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
--- quizius-maximus/src/views/Modules.vue
+++ quizius-maximus/src/views/Modules.vue
@@ -35,7 +35,7 @@ const filteredModules = computed(() => {
 <template>
     <main>
         <!-- Kopfbereich -->
-        <section class="py-4 text-center container">
+        <section class="pt-4 text-center container">
             <div class="row py-lg-3">
                 <div class="col-lg-6 col-md-8 mx-auto">
                     <h1 class="fw-light">Module</h1>
@@ -43,44 +43,16 @@ const filteredModules = computed(() => {
                         <input type="search" class="form-control" v-model="search"
                             placeholder="Suche nach einem Modul für ein Quiz..." list="datalistOptions"
                             id="exampleDataList">
-                        <!-- TODO: Testen mit mehr als 2 bzw. 3 und mehr Modulen -->
                         <datalist id="datalistOptions">
                             <option v-for="module in filteredModules.slice(0, 3)"
-                                :value="module.shortname + ': ' + module.longname"></option>
+                                :value="module.longname"></option>
                         </datalist>
-                    </p>
-                    <p class="lead text-body-secondary">
-                        <span class="btn btn-outline-danger">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                                class="bi bi-person-fill" viewBox="0 0 16 16">
-                                <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6">
-                                </path>
-                            </svg>
-                        </span>
-                        führt zur Auswahl
-                        <span class="text-danger"> kompetitiver </span>
-                        Spielmodi
-                    </p>
-                    <p class="lead text-body-secondary">
-                        <span class="btn btn-outline-success">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                                class="bi bi-people-fill" viewBox="0 0 16 16">
-                                <path
-                                    d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6m-5.784 6A2.24 2.24 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.3 6.3 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1zM4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5">
-                                </path>
-                            </svg>
-                        </span>
-                        führt zur Auswahl
-                        <span class="text-success">
-                            kooperativer
-                        </span>
-                        Spielmodi
                     </p>
                 </div>
             </div>
         </section>
         <!-- Liste der Module als Cards -->
-        <section class="album py-3 container">
+        <section class="album container">
             <div class="row row-cols-1 row-cols-lg-2 row-cols-xxl-3 g-3">
                 <ModuleCard v-for="module in filteredModules" :moduleid="module.id" :shortname="module.shortname"
                     :longname="module.longname" />
