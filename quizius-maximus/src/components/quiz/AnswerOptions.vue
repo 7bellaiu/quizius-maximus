831ce8ad2c388f4bab439681e0bda76a3e0af267
Daniela Bodner <Daniela.Bodner@iu-study.org>
2025-01-25 18:16:11 +0100

    SchnellComp: Auswahl und Färbung korrekte Antwort

--- quizius-maximus/src/components/quiz/AnswerOptions.vue
+++ quizius-maximus/src/components/quiz/AnswerOptions.vue
@@ -4,6 +4,10 @@ const props = defineProps({
     options: {
         type: Array,
         required: true
+    },
+    display: {
+        type: Boolean,
+        required: true
     }
 });
 
@@ -21,14 +25,32 @@ const selectAnswer = (index) => {
 <template>
     <div class="row row-cols-2 mx-2 mt-2">
         <div v-for="(option, index) in options" :key="index" class="col">
-            <button type="button" class="btn p-0 my-1 w-100" :class="{
+            <!-- Nicht-klickbar nach Spieler-Auswahl -->
+            <div v-if="props.display" class="btn p-0 my-1 w-100" :class="{
+                'btn-outline-primary': !option.selected,
+                'btn-outline-success': option.correct && option.selected,
+                'btn-outline-danger': !option.correct && option.selected
+            }">
+                <div class="card" :class="{
+                    'border-primary': !option.selected,
+                    'border-success text-success': option.correct && option.selected,
+                    'bg-danger text-white': !option.correct && option.selected
+                }">
+                    <div class="card-body">
+                        <p class="card-text">{{ option.text }}</p>
+                    </div>
+                </div>
+            </div>
+
+            <!-- klickbar vor Spieler-Auswahl -->
+            <button v-else type="button" class="btn p-0 my-1 w-100" :class="{
                 'btn-outline-primary': !option.selected,
                 'btn-outline-success': option.correct && option.selected,
                 'btn-outline-danger': !option.correct && option.selected
             }" @click="selectAnswer(index)">
                 <div class="card" :class="{
                     'border-primary': !option.selected,
-                    'border-success': option.correct && option.selected,
+                    'border-success text-success': option.correct && option.selected,
                     'bg-danger text-white': !option.correct && option.selected
                 }">
                     <div class="card-body">
--- quizius-maximus/src/components/quiz/Quiz.vue
+++ quizius-maximus/src/components/quiz/Quiz.vue
@@ -2,12 +2,14 @@
 import AnswerOptions from '@/components/quiz/AnswerOptions.vue';
 import Progress from '@/components/quiz/Progress.vue';
 import Question from '@/components/quiz/Question.vue';
-import { ref, computed } from 'vue';
+import { computed, ref } from 'vue';
 
 const props = defineProps(["questions", "currentQuestion", "totalQuestions", "gameMode"]);
+const isAnswered = ref(false); // Flag, um zu wissen, ob der Spieler bereits geantwortet hat 
+const isAnswerCorrect = ref(false);
 
 const questionText = computed(() => props.questions[props.currentQuestion]?.question || "Keine Frage gefunden");
-const answerOptions = computed(() => {
+const answerOptionsPreClick = computed(() => {
     const q = props.questions[props.currentQuestion];
     const options = [
         { text: q?.option1, selected: false, correct: q?.correctAnswer === 'option1' },
@@ -18,11 +20,19 @@ const answerOptions = computed(() => {
     return options;
 });
 
-// Handhabung der Antwortwahl
-const handleAnswerSelected = (index) => {
-    const selectedOption = answerOptions.value[index];
-    // TODO: Verarbeitungslogik, um zu prüfen, ob die Antwort korrekt ist.
-};
+const answerOptionsPostClick = ref([]);
+
+const handleAnswer = (index) => {
+    const q = props.questions[props.currentQuestion];
+    answerOptionsPostClick.value = [
+        { text: q?.option1, selected: index == 0, correct: q?.correctAnswer === 'option1' },
+        { text: q?.option2, selected: index == 1, correct: q?.correctAnswer === 'option2' },
+        { text: q?.option3, selected: index == 2, correct: q?.correctAnswer === 'option3' },
+        { text: q?.option4, selected: index == 3, correct: q?.correctAnswer === 'option4' }
+    ];
+
+    isAnswered.value = true;
+}
 </script>
 
 <template>
@@ -35,6 +45,9 @@ const handleAnswerSelected = (index) => {
         <Question v-if="questionText" :questionText="questionText" />
 
         <!-- Antwortoptionen -->
-        <AnswerOptions v-if="answerOptions.length > 0" :options="answerOptions" />
+        <AnswerOptions v-if="answerOptionsPreClick.length > 0 && !isAnswered" :options="answerOptionsPreClick"
+            :display="false" @answer-selected="handleAnswer" />
+        <AnswerOptions v-else-if="answerOptionsPostClick.length > 0 && isAnswered" :options="answerOptionsPostClick"
+            :display="true" />
     </section>
 </template>
